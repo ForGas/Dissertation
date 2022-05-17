2b31bb05382be7da40e1baffa3fd9b9743f9d56d
ForGas <lieber_kz@mail.ru>
2022-05-18 03:41:08 +0600

    refactor minor

--- clientapp/src/components/hooks/useFetch.js
+++ clientapp/src/components/hooks/useFetch.js
@@ -1,21 +1,22 @@
 import { useState, useEffect } from "react";
 
-export const useFetch = uri => {
+export function useFetch(uri) {
     const [data, setData] = useState();
     const [error, setError] = useState();
     const [loading, setLoading] = useState(true);
 
+    console.log(uri);
+
     useEffect(() => {
+        if (!uri) return;
         fetch(uri, {
-            referrer: "",
             url: '/',
-            referrerPolicy: 'no-referrer',
         })
             .then(response => response.json())
             .then(setData)
             .then(() => setLoading(false))
             .cath(setError)
-    }, [])
+    }, [uri])
 
     return {
         loading,
--- clientapp/src/components/pages/Footer.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react"
-
-export const Footer = () =>
-    <footer className="page-footer font-small blue pt-4">
-        <div className="footer-copyright text-center py-3">© 2022 Copyright</div>
-    </footer>
