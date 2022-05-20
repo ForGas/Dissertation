6f2967965db2041f56216be833f32e22b4016d17
ForGas <lieber_kz@mail.ru>
2022-05-20 23:05:56 +0600

    add network incident

---
+++ Dissertation/Persistence/Entities/Network/NetworkIncident.cs
@@ -0,0 +1,10 @@
+﻿using Dissertation.Persistence.Entities.Common;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Dissertation.Persistence.Entities;
+
+public class NetworkIncident : BaseIncident
+{
+    [NotMapped]
+    public override IncidentType TypeName => IncidentType.Network;
+}
