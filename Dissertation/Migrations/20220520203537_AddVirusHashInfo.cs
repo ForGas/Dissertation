8b8f9c577077af4fd2c0ff7d0b2b8a7a7b5893c5
ForGas <lieber_kz@mail.ru>
2022-05-21 23:51:31 +0600

    add migration for file virus hash info

---
+++ Dissertation/Migrations/20220520203537_AddVirusHashInfo.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dissertation.Migrations
+{
+    public partial class AddVirusHashInfo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Sha256",
+                table: "FileDetails",
+                type: "nvarchar(64)",
+                maxLength: 64,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(3000)",
+                oldMaxLength: 3000);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Sha1",
+                table: "FileDetails",
+                type: "nvarchar(40)",
+                maxLength: 40,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(3000)",
+                oldMaxLength: 3000);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Md5",
+                table: "FileDetails",
+                type: "nvarchar(32)",
+                maxLength: 32,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(3000)",
+                oldMaxLength: 3000);
+
+            migrationBuilder.CreateTable(
+                name: "VirusHashInfo",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    Source = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    Md5 = table.Column<string>(type: "nvarchar(32)", maxLength: 32, nullable: true),
+                    Sha1 = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: true),
+                    Sha256 = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: false),
+                    IsVirus = table.Column<bool>(type: "bit", nullable: false),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastModified = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VirusHashInfo", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VirusHashInfo_Sha256",
+                table: "VirusHashInfo",
+                column: "Sha256",
+                filter: "[Sha256] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VirusHashInfo");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Sha256",
+                table: "FileDetails",
+                type: "nvarchar(3000)",
+                maxLength: 3000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(64)",
+                oldMaxLength: 64);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Sha1",
+                table: "FileDetails",
+                type: "nvarchar(3000)",
+                maxLength: 3000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(40)",
+                oldMaxLength: 40);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Md5",
+                table: "FileDetails",
+                type: "nvarchar(3000)",
+                maxLength: 3000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(32)",
+                oldMaxLength: 32);
+        }
+    }
+}
